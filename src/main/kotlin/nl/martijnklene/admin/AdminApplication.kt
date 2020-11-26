e4c4f8fc7ed1d807b37cb223aa1877773d07896c
Martijn Klene <martijn.klene@voiceworks.com>
2020-11-26 16:39:35 +0100

    Initial commit with Spring in it

---
+++ src/main/kotlin/nl/martijnklene/admin/AdminApplication.kt
@@ -0,0 +1,11 @@
+package nl.martijnklene.admin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdminApplication
+
+fun main(args: Array<String>) {
+    runApplication<AdminApplication>(*args)
+}
