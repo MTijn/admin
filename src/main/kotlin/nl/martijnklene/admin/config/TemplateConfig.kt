790dcc513680fc4b0889113c6da537aee8172f8c
Martijn Klene <schapie.nl@gmail.com>
2020-12-10 17:48:58 +0100

    Disabled the MVC thingie, now the dates are perfect

--- src/main/kotlin/nl/martijnklene/admin/config/TemplateConfig.kt
+++ src/main/kotlin/nl/martijnklene/admin/config/TemplateConfig.kt
@@ -2,13 +2,11 @@ package nl.martijnklene.admin.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer
 
 @Configuration
-@EnableWebMvc
 class TemplateConfig : WebMvcConfigurer {
     override fun configureViewResolvers(registry: ViewResolverRegistry) {
         registry.freeMarker()
