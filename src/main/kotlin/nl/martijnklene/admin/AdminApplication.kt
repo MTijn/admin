c87089ede3ee4bd8efb4db11163093de931db2dc
Martijn Klene <schapie.nl@gmail.com>
2020-12-11 17:26:27 +0100

    Include different config files, and release it, more or less.

--- src/main/kotlin/nl/martijnklene/admin/AdminApplication.kt
+++ src/main/kotlin/nl/martijnklene/admin/AdminApplication.kt
@@ -2,8 +2,14 @@ package nl.martijnklene.admin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.PropertySource
+import org.springframework.context.annotation.PropertySources
 
 @SpringBootApplication
+@PropertySources(
+    PropertySource("classpath:application.yaml"),
+    PropertySource("file:/run/secrets/blog_config", ignoreResourceNotFound = true)
+)
 class AdminApplication
 
 fun main(args: Array<String>) {
