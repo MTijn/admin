5698f48a807aba7ceb7051a6ecb98536ff986738
Martijn Klene <martijn.klene@enreach.com>
2023-01-05 09:27:12 +0100

    Made an oops in the filter chain

--- src/main/kotlin/nl/martijnklene/admin/SecurityConfig.kt
+++ src/main/kotlin/nl/martijnklene/admin/SecurityConfig.kt
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @Configuration
@@ -19,7 +20,7 @@ class SecurityConfig {
     }
 
     @Bean
-    fun filterChain(http: HttpSecurity) {
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http
             .csrf()
             .disable()
@@ -34,5 +35,6 @@ class SecurityConfig {
             .clearAuthentication(true)
             .deleteCookies("JSESSIONID")
             .logoutRequestMatcher(AntPathRequestMatcher("/logout"))
+        return http.build()
     }
 }
