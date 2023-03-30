a3db16e09637300121f53ddd6bfc5483af5f122e
Martijn Klene <martijn.klene@enreach.com>
2023-03-30 15:21:31 +0200

    Using forwarded headers in a filter and upgraded packages

--- src/main/kotlin/nl/martijnklene/admin/config/WebConfig.kt
+++ src/main/kotlin/nl/martijnklene/admin/config/WebConfig.kt
@@ -1,12 +1,24 @@
 package nl.martijnklene.admin.config
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
 import org.springframework.core.io.ClassPathResource
+import org.springframework.web.filter.ForwardedHeaderFilter
 import org.springframework.web.servlet.function.RouterFunctions.resources
+
 
 @Configuration
 class WebConfig {
     @Bean
     fun resRouter() = resources("/**", ClassPathResource("static/"))
+
+    @Bean
+    fun forwardedHeaderFilter(): FilterRegistrationBean<ForwardedHeaderFilter>? {
+        val filterRegistrationBean = FilterRegistrationBean<ForwardedHeaderFilter>()
+        filterRegistrationBean.filter = ForwardedHeaderFilter()
+        filterRegistrationBean.order = Ordered.HIGHEST_PRECEDENCE
+        return filterRegistrationBean
+    }
 }
