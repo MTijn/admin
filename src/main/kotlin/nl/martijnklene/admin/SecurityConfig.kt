50c6dfe7a3c4e579a50fbe6bbe2f5e3d1f880cf5
Martijn Klene <schapie.nl@gmail.com>
2025-04-17 20:31:59 +0200

    Upgraded packages for admin.

--- src/main/kotlin/nl/martijnklene/admin/SecurityConfig.kt
+++ src/main/kotlin/nl/martijnklene/admin/SecurityConfig.kt
@@ -3,39 +3,30 @@ package nl.martijnklene.admin
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.builders.WebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfig {
     @Bean
-    fun webSecurityCustomizer(): WebSecurityCustomizer? =
-        WebSecurityCustomizer { web: WebSecurity ->
-            web
-                .ignoring()
-                .requestMatchers("/v1/**")
-        }
-
-    @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        http
-            .csrf()
-            .disable()
-            .authorizeHttpRequests()
-            .requestMatchers("/v1/**")
-            .permitAll()
-            .requestMatchers("/**")
-            .authenticated()
-            .and()
-            .oauth2Login()
-            .and()
-            .logout()
-            .invalidateHttpSession(true)
-            .clearAuthentication(true)
-            .deleteCookies("JSESSIONID")
-            .logoutRequestMatcher(AntPathRequestMatcher("/logout"))
+        http {
+            csrf { disable() }
+            authorizeHttpRequests {
+                authorize("/v1/**", permitAll)
+                authorize("/**", authenticated)
+            }
+            oauth2Login {}
+            logout {
+                invalidateHttpSession = true
+                clearAuthentication = true
+                deleteCookies("JSESSIONID")
+                logoutRequestMatcher = AntPathRequestMatcher("/logout")
+            }
+        }
         return http.build()
     }
 }
