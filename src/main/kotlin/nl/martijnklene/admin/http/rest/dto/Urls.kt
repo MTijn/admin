13b09ae067af4d8e93deccbecce4e1ead8037dc9
Martijn Klene <martijn.klene@enreach.com>
2025-05-06 14:01:44 +0200

    Google URLset

--- src/main/kotlin/nl/martijnklene/admin/http/rest/dto/Urls.kt
+++ src/main/kotlin/nl/martijnklene/admin/http/rest/dto/Urls.kt
@@ -3,7 +3,7 @@ package nl.martijnklene.admin.http.rest.dto
 import com.fasterxml.jackson.annotation.JsonRootName
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper
 
-@JsonRootName("urls", namespace = "http://www.sitemaps.org/schemas/sitemap/0.9")
+@JsonRootName("urlset", namespace = "http://www.sitemaps.org/schemas/sitemap/0.9")
 class Urls(
     @JacksonXmlElementWrapper(useWrapping = false)
     val url: List<Url>,
